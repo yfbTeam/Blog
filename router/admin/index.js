5864ece8c3b91cb69013b8f6494be05acb9a8051
liuzeaa <1098367891@qq.com>
2017-12-25 23:35:58 +0800

    简化代码

--- router/admin/index.js
+++ router/admin/index.js
@@ -1,26 +1,15 @@
 var express = require("express");
-
 var router = express.Router();
-
-var userRouter = require('./user')
-var tagRouter = require('./tag')
-var typeRouter  =require('./artType')
-var attributeRouter = require('./attribute')
-var articleRouter = require('./article')
 //当我们访问/admin之后，全部都会进入到这个模块来
-
 router.use("/",function(req,res,next){ 
     next();
 });
-
 router.get("/",function(req,res,next){
     res.render("admin/index",{});    
 });
-
-router.use(userRouter)
-router.use(tagRouter)
-router.use(typeRouter)
-router.use(attributeRouter)
-router.use(articleRouter)
-
+router.use(require('./user'))
+router.use(require('./tag'))
+router.use(require('./artType'))
+router.use(require('./attribute'))
+router.use(require('./article'))
 module.exports = router;
